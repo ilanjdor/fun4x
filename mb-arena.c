afa6c2aa45dadd40b4a5e525aa5bf7b2e629d74f
vagrant <vagrant@CS61-Appliance>
2016-09-30 02:55:28 +0000

    video solution working

--- mb-arena.c
+++ mb-arena.c
@@ -21,46 +21,56 @@ See "membench.h" for function semantics.
 
 */
 
+typedef union chunk_or_free {
+    chunk c;
+    union chunk_or_free* next;
+} chunk_or_free;
 
 #define GROUPSIZE 4096
 typedef struct membench_group {
-    int pos;
-    chunk chunks[GROUPSIZE];
+    chunk_or_free chunks[GROUPSIZE];
     struct membench_group* next;
 } membench_group;
 
 struct membench_arena {
     membench_group* group;
+    chunk_or_free* free_list;
 };
 
 
-static membench_group* membench_group_new(void) {
+static membench_group* membench_group_new(membench_arena* a) {
     membench_group* g = (membench_group*) malloc(sizeof(membench_group));
-    g->pos = 0;
+    for (size_t i = 0; i < GROUPSIZE; i++) {
+        g->chunks[i].next = a->free_list;
+        a->free_list = &g->chunks[i];
+    }
     return g;
 }
 
 membench_arena* membench_arena_create(void) {
     membench_arena* arena = (membench_arena*) malloc(sizeof(membench_arena));
-    arena->group = membench_group_new();
+    arena->free_list = NULL;
+    arena->group = membench_group_new(arena);
     return arena;
 }
 
 chunk* membench_alloc(membench_arena* arena) {
-    membench_group* g = arena->group;
-    if (g->pos == GROUPSIZE) {
+    if (!arena->free_list) {
         // allocate new group, add it to singly-linked group list
-        g = membench_group_new();
+        membench_group* g = membench_group_new(arena);
         g->next = arena->group;
         arena->group = g;
     }
-    chunk* result = &g->chunks[g->pos];
-    ++g->pos;
+
+    chunk* result = &arena->free_list->c;
+    arena->free_list = arena->free_list->next;
     return result;
 }
 
 void membench_free(membench_arena* arena, chunk* x) {
-    (void) arena, (void) x;
+    chunk_or_free* cf = (chunk_or_free*) x;
+    cf->next = arena->free_list;
+    arena->free_list = cf;
 }
 
 void membench_arena_destroy(membench_arena* arena) {
